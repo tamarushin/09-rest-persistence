fd6a71b99b2c123fa341cdb3b049e6059a1f7be0
tamarushin <tamarushin@gmail.com>
2018-06-19 15:07:35 -0700

    finished writing tests

--- src/__test__/api.test.js
+++ src/__test__/api.test.js
@@ -1,8 +1,8 @@
-use strict';
+'use strict';
 
 const superagent = require('superagent');
 const app = require('../app.js');
-const uuid = require('uuid/v4');
+// const uuid = require('uuid/v4');
 
 describe('Simple Proof of Life', () => {
   beforeAll(() => {
@@ -12,62 +12,99 @@ describe('Simple Proof of Life', () => {
     app.stop();
   });
 
-  // it('handles an invalid GET request if the route is not the correct route with a 404', () => {
+  it('handles an invalid GET request if the route is not the correct route with a 404', () => {
 
-  //   return superagent.get('http://localhost:3000/?month=June')
-  //     .then( () => {
-  //       expect(false).toBeTruthy(); //help from John
-  //     })
-  //     .catch(response => {
-  //       expect(response.status).toEqual(404);
-  //     });
-  // });
-
-  it('handles a GET request with a valid ID', () => {
-    const id = 3232;
-    return superagent.get(`http://localhost:3000/api/v1/password/?id=${id}`)
-      .then(response => {
-        expect(response.status).toEqual(200);
-        expect(response.text).toEqual(`Please enter your password ${id} now`);
+    return superagent.get(`http://localhost:3000/api/v1`)
+      .then( () => {
+        expect(false).toBeTruthy(); //help from John
+      })
+      .catch(response => {
+        expect(response.status).toEqual(404);
       });
   });
 
-//   it('handles an invalid GET request with an empty ID', () => {
-//     return superagent.get(`http://localhost:3000/api/v1/password/?id=${id}`)
-//       .catch(response => {
-//         expect(response.status).toEqual(400);
-//         expect(response.toString()).toEqual('Error: Bad Request');
-//       });
-//   });
+  it('handles a GET request with a valid ID', () => {
+    const id = 3232;
+    return superagent.get(`http://localhost:3000/api/v1/magazines/?id=${id}`)
+      .then(response => {
+        expect(response.statusCode).toEqual(200);
+        expect(response.text).toEqual(`magazine number ${id} is needed`);
+      });
+  });
 
-//   it('handles a GET request with ID that was not found', () => {
-//     let id = uuid();
-//     return superagent.get(`http://localhost:3000/api/v1/password/?id=${id}`)
-//       .catch(response => {
-//         expect(response.status).toEqual(404);
-//         expect(response.toString()).toEqual('Error: ID Not Found');
-//       });
-//   });
+  it('handles an invalid GET request with an empty ID', () => {
+    return superagent.get(`http://localhost:3000/api/v1/magazines/?id=`)
+      .catch(response => {
+        expect(response.status).toEqual(400);
+        expect(response.toString()).toEqual('Error: Bad Request');
+        //Expect response to be the string 'Error: Bad Request'
+      });
+  });
 
-//   it('handles a good POST request', () => {
-//     let obj = { name: 'Tama' };
-//     let expect = JSON.stringify(obj);
-//     return superagent.post('http://localhost:3000/api/v1/password/')
-//       .send(obj)
-//       .then(response => {
-//         expect(response.toString()).toEqual(200);
-//       });
-//   });
+  it('handles a good POST request with JSON', () => {
+    let obj = { content: 'The true tales of Heroine Tama' };
+    return superagent.post(`http://localhost:3000/api/v1/magazines/`)
+      .send(obj)
+      .then(response => {
+        expect(response.status.toEqual(200));
+        expect(JSON.parse(response.text)).toEqual(obj);('The true tales of the Heroine Tama');
+      });
+  });
 
-//   it('handles an invalid POST request', () => {
-//     let obj = {};
-//     let expect = JSON.stringify(obj);
-//     return superagent.post('http://localhost:3000/api/v1/password/')
-//       .send(obj)
-//       .catch(response => {
-//         expect(response.statusCode).toEqual(400);
-//         expect(response.toString()).toEqual('Error: Bad, Bad, Bad Request');
-//       });
-//   }); 
-// });
+  it('handles an invalid POST request', () => {
+    let obj = {}; //empty object passed
+    return superagent.post(`http://localhost:3000/api/v1/magazines/`)
+      .send(obj)
+      .catch(response => {
+        expect(response.status).toEqual(400);
+        expect(response.toString()).toEqual('Error: Bad Request');
+      });
+  });
+
+  it('handles a GET request with ID that was not found', () => {
+    return superagent.get(`http://localhost:3000/api/v1/password/?id=<uuid>`)
+      .catch(response => {
+        expect(response.status).toEqual(404);
+        expect(response.toString()).toEqual('Error: ID Not Found');
+      });
+  });
+  
+  it('handles a DELETE request with a valid ID', () => {
+    let id = 3232; //magazine with a valid ID
+    return superagent.delete(`http://localhost:3000/api/v1/magazines?id=${id}`)
+      .then(response => {
+        expect(response.statusCode).toEqual(204);
+        expect(response.text).toEqual('');
+      });
+  });
+  it('handles an invalid DELETE request with no valid ID', () => {
+    return superagent.delete(`http://localhost:3000/api/v1/magazines/?id=<uuid>`)
+      .catch(response => {
+        expect(response.status).toEqual(400);
+        expect(response.toString()).toEqual('Error: Bad Request');
+      });
+  });
+
+  it('handles a good PUT request with JSON', () => {
+    let obj = { name: 'Tama', content: 'Super Heroine'};
+    return superagent.put(`http://localhost:3000/api/v1/password/`)
+      .send(obj)
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(JSON.parse(response.text)).toEqual(obj);
+        //console.log(obj.name); //expected result: Tama
+        //console.log(obj.content); //expected result: Super Heroine
+      });
+  }); //Parse is to break apart object
+
+  it('handles an invalid PUT request', () => {
+    let obj = {}; //empty object passed in
+    return superagent.put(`http://localhost:3000/api/v1/magazine/`)
+      .send(obj)
+      .catch(response => {
+        expect(response.status).toEqual(400);
+        expect(response.toString()).toEqual('Error: Bad Request');
+      });
+  });
+});
 
